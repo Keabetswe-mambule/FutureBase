7f48f94683cd85b6e74ac6a3bce9e3db5baddd5f
KEABETSWE MAMBULE <king.keabe@gmail.com>
2021-03-11 15:38:39 +0200

    Add README.md and added CRUD methods structure with no functionality

---
+++ README.md
@@ -0,0 +1,17 @@
+# What is FutureBase
+
+# How to use FutureBase?
+
+add installation and dependencies here
+
+# Features
+
+what makes FutureBase stand out
+
+# Contribute
+
+futurebase code, how it was writen
+
+# Credits
+
+# License
--- index.js
+++ index.js
@@ -1,1 +1,93 @@
-function futurebase() {}
+//only 1 database instance is stored
+let baseInstance = {};
+let current_base = null;
+const FutureBase = {
+  //create JSON file to hold data
+  createBase: (base_name) => {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  },
+
+  deleteBase: (base_name) => {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  },
+
+  //should accept path relating to object structure [to update and delete from a single path of a base]
+  writeToBase: (base_path_string) => {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  },
+
+  //should accept path relating to object structure [to update and delete from multple paths of a base]
+  writeMultipleToBase: (...comma_separated_list_of_paths_strings) => {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  },
+
+  //should accept path relating to object structure [to read from a single path of a base]
+  readFromBase: (base_path_string) => {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  },
+
+  //should accept path relating to object structure [to read from multiple paths of a base]
+  readMultipleFromBase: (...comma_separated_list_of_paths_strings) => {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  },
+
+  //returns an object with the structure of a base
+  getBaseInstance(base_name) {
+    return new Promise((resolve, reject) => {
+      if (somethingSuccesfulHappend) {
+        resolve(baseInstance);
+      } else {
+        reject();
+      }
+    });
+  },
+
+  //returns currently highlighted base
+  getCurrentBase() {
+    return new Promise((resolve, reject) => {
+      if (current_base != null) {
+        resolve(current_base);
+      } else {
+        reject("No base highlighted");
+      }
+    });
+  },
+};
+
+modules.exports.FutureBase = FutureBase;
